c712cc8d84d228891d0831fab607a1c87d4f5278
Natasha Smith <pisceanchic@gmail.com>
2021-06-23 21:06:25 +0200

    progress so far

--- js/index.js
+++ js/index.js
@@ -31,37 +31,6 @@ return subtotal;
 
  */
 
-//Step 5: In step 3, you calculated the subtotal price, and in step 4, 
-//you got the DOM element that should have displaying this price. 
-//In this step, your goal is to set the subtotal price to the 
-//corresponding DOM element. Also, make sure this function returns the 
-//value of subtotal so you can use it later when needed.
-
-/*
-const $cart = document.querySelector('#cart tbody');
-const $calc = document.getElementById('calc');
-
-function updateSubtot($product) {
-  // Iteration 1.1
-  const price = product.querySelector('.price span');
-
-              const priceValue = priceSpan.innerText;
-
-  const quantityInput = product.querySelector('input');
-
-              const quantityValue = quantityInput.value;
-
-
-  const subtotal = parseFloat(priceValue) * parseInt(quantityValue);
-
-  const subtotalSpan = product.querySelector('.subtotal span');
-
-  subtotalSpan.innerText = subtotal.toFixed(2);
-
-  return subtotal;
-}
-*/
-
 }
 
 function calculateAll() {
